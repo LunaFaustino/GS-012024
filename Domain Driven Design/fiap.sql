474087c8f4cca50b49f953b25adaa1a9e0add83c
LunaFaustino <lu.faustino2000@gmail.com>
2024-06-04 13:30:30 -0300

    Fazendo entrega de JAVA

--- Domain Driven Design/fiap.sql
+++ Domain Driven Design/fiap.sql
@@ -13,15 +13,26 @@ create table t_empresa(
     foreign key (cpf) references t_representante (cpf)
 );
 
-create table t_contato(
+select * from t_empresa;
+drop table t_empresa;
+
+create table t_contatopf(
     email varchar2(100),
-    cel varchar2(15),
     tel varchar2(15),
     cpf varchar2(14),
-    foreign key (cpf) references t_representante (cpf),
+    foreign key (cpf) references t_representante (cpf)
+);
+
+drop table t_contatopf;
+
+create table t_contatopj(
+    email varchar2(100),
+    tel varchar2(15),
     cnpj varchar2(18),
     foreign key (cnpj) references t_empresa (cnpj)
 );
+
+drop table t_contatopj;
 
 create table t_produto(
     tipo varchar2(100),
@@ -31,9 +42,11 @@ create table t_produto(
     foreign key (cnpj) references t_empresa (cnpj)
 );
 
+drop table t_produto;
+
 create table t_endereco(
     numero int,
-    cep varchar2(8),
+    cep varchar2(9),
     logradouro varchar2(100),
     complemento varchar2(50),
     bairro varchar2(50),
@@ -43,8 +56,21 @@ create table t_endereco(
     foreign key (cnpj) references t_empresa (cnpj)
 );
 
-create table t_cadastro(
+drop table t_endereco;
+
+create table t_cadastropf(
     nome varchar2(100),
     email varchar2(100),
     cep varchar2(9)
 );
+
+drop table t_cadastropf;
+
+create table t_cadastropj(
+    cnpj varchar2(18),
+    nomeFant varchar2(100),
+    email varchar2(100),
+    cep varchar2(9)
+);
+
+drop table t_cadastropj;
