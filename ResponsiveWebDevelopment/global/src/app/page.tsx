444eaea191068ca64bcb5e1cd96a679076187b1f
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-07 09:08:12 -0300

    Fazendo GS de Front

--- ResponsiveWebDevelopment/global/src/app/page.tsx
+++ ResponsiveWebDevelopment/global/src/app/page.tsx
@@ -1,6 +1,12 @@
 import "../../public/css/home.css"
 import Image from "next/image";
 import plasticos from "../../public/assets/plasticos-removebg.png"
+import login from "../../public/assets/Login.png"
+import home from "../../public/assets/Home-Celularhome.png"
+import scan from "../../public/assets/Scan-Celular.png"
+import descricao from "../../public/assets/Descrição-Celular.png"
+import localizacao from "../../public/assets/Localização-Celular.png"
+import catador from "../../public/assets/Comunidade-Celular.png"
 import Link from "next/link";
 
 const Home = () => {
@@ -21,8 +27,24 @@ const Home = () => {
         </article>
         <Image src={plasticos} width={400} height={300} alt="Embalagens de plástico"></Image>
       </section>
-
-      <h2 className="titulo">Mas isso tudo é muita informação para decorar, né?! Por isso o nosso aplicativo XXX está aqui para te ajudar!</h2>
+      <h2 className="titulo">Mas isso tudo é muita informação para decorar, né?! Por isso o nosso aplicativo <span className="marcar">SustentApp</span> está aqui para te ajudar!</h2>
+      <div>
+        <section className="section">
+          <Image src={login} width={400} height={400} alt=""></Image>
+          <p>Você realiza o seu login e então terá acesso ao sistema de Scan, além de notícias e outras informações sobre reciclagem.</p>
+          <Image src={home} width={400} height={400} alt=""></Image>
+        </section>
+        <section className="section">
+          <Image src={scan} width={400} height={400} alt=""></Image>
+          <p>Após escanear o selo do material ou o código de barras, aparecerá várias informações sobre o material. Como se é reciclável ou não, como reciclar e orientações de higiene.</p>
+          <Image src={descricao} width={400} height={400} alt=""></Image>
+        </section>
+        <section className="section">
+          <Image src={localizacao} width={400} height={400} alt=""></Image>
+          <p>Após isso, por meio da sua localização é possível encontrar cooperativas e catadores próximos. Caso escolha um catador, poderá combinar uma coleta.</p>
+          <Image src={catador} width={400} height={400} alt=""></Image>
+        </section>
+      </div>
       <h2 className="titulo">Gostou do nosso aplicativo? Já quer garantir seu lugar ajudando a natureza? <Link href="/reciclagem">Então vem cá!</Link></h2>
     </>
   )
